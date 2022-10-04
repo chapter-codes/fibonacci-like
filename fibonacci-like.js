4da6a41a98fe53cd502faf0d25d2e682263e4944
Churchill Nnanna <68286428+chapter-codes@users.noreply.github.com>
2022-10-04 22:06:09 +0100

    Updated fibonnaci.js

--- fibonacci-like.js
+++ fibonacci-like.js
@@ -1,3 +1,5 @@
+'use strict'
+
 //A fibonnaci like function: takes 2 numbers and make a fibonnacci-like array from them. the third number  specifies the array length.
 
 function fiblike(a, b, count){
@@ -9,5 +11,21 @@ function fiblike(a, b, count){
 	return fibArray.reverse();
 }
 
+
+//function which checks if an array of  numbers is  fibonnacci-like.
+function fiblikeChecker(arr){
+	const [a,b]=arr
+	const check=fiblike(a,b,arr.length)
+	const isFiblike=arr.every((item,index)=>item==check[index])
+
+
+	return isFiblike
+}
+
 const arr=fiblike(2,3,5)
 console.log(arr)
+//[ 2, 3, 5, 8, 13 ]
+console.log(fiblikeChecker([1,2,3,4,5,5,6]))
+//false
+console.log(fiblikeChecker(arr))
+//true
